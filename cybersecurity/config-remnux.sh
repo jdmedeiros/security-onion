a0bd8120f5e08e1192ccaa7c93efba48eeed4edf
Dan Medeiros <dan.medeiros@sagewell.com>
2023-03-21 10:52:28 -0100

    Security Onion, Analyst VM, Kali and REMnu

---
+++ cybersecurity/config-remnux.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+  if [ "$1" = "run" ]; then
+  LOGFILE="/var/log/cloud-config-"$(date +%s)
+  SCRIPT_LOG_DETAIL="$LOGFILE"_$(basename "$0").log
+
+  # Reference: https://serverfault.com/questions/103501/how-can-i-fully-log-all-bash-scripts-actions
+  exec 3>&1 4>&2
+  trap 'exec 2>&4 1>&3' 0 1 2 3
+  exec 1>$SCRIPT_LOG_DETAIL 2>&1
+
+  export DEBIAN_FRONTEND=noninteractive
+  export DEBCONF_NONINTERACTIVE_SEEN=true
+  apt-get -o DPkg::Options::=--force-confdef update
+  apt-get -y -o DPkg::Options::=--force-confdef upgrade
+
+  cp /var/lib/cloud/instance/scripts/NetworkMiner.desktop /usr/share/applications/NetworkMiner.desktop
+  chmod 777 /usr/share/applications/NetworkMiner.desktop
+
+fi
