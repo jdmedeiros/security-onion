6575c3917b10b801fdd0d4121eadba5e22cbd4cf
Dan Medeiros <dan.medeiros@sagewell.com>
2023-03-21 10:47:13 -0100

    Security Onion, Analyst VM, Kali and REMnu

---
+++ cybersecurity/templates.tf
@@ -0,0 +1,35 @@
+data "template_file" "fstab" {
+  template = file("${path.module}/update-fstab.tpl")
+
+  vars = {
+    onion_ip = aws_network_interface.onion_nic_private1.private_ip,
+    efs_ip = aws_efs_mount_target.onion2-mnt1.ip_address
+  }
+}
+
+data "template_file" "kali-password" {
+  template = file("${path.module}/kali-change-password.tpl")
+
+  vars = {
+    userid = "kali",
+    userpw = var.kali_userpw
+  }
+}
+
+data "template_file" "desktop-password" {
+  template = file("${path.module}/desktop-change-password.tpl")
+
+  vars = {
+    userid = "ubuntu",
+    userpw = var.desktop_userpw
+  }
+}
+
+data "template_file" "remnux-password" {
+  template = file("${path.module}/remnux-change-password.tpl")
+
+  vars = {
+    userid = "remnux",
+    userpw = var.remnux_userpw
+  }
+}
